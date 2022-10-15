2d86aef2802dee6c20e35b3401f4917f66e5a51d
BatParkour <minecrafthenn@gmail.com>
2022-10-15 17:20:21 +0300

    added navbar

--- src/components/user.vue
+++ src/components/user.vue
@@ -168,6 +168,48 @@ function runRender(){
 
 
   <div key='User' v-else-if="userdata.exists == true">
+    <nav>
+		<menu>
+      <menuitem>
+        <a>Skywars</a>
+        <menu>
+          <menuitem>
+            <a>Overall</a>
+            <menu>
+               <menuitem><a>Normal</a></menuitem>
+               <menuitem><a>Insane</a></menuitem>
+            </menu>  
+          </menuitem>
+          <menuitem>
+            <a>Solos</a>
+            <menu>
+               <menuitem><a>Normal</a></menuitem>
+               <menuitem><a>Insane</a></menuitem>
+            </menu>  
+          </menuitem>
+          <menuitem>
+            <a>Doubles</a>
+            <menu>
+               <menuitem><a>Normal</a></menuitem>
+               <menuitem><a>Insane</a></menuitem>
+            </menu>  
+          </menuitem>
+        </menu>
+      </menuitem>
+			<menuitem>
+				<a>BedWars</a>
+				<menu>
+					<menuitem><a>Core</a></menuitem>
+          <menuitem><a>Duos</a></menuitem>
+          <menuitem><a>Trios</a></menuitem>
+          <menuitem><a>Fours</a></menuitem>
+          <menuitem><a>4v4</a></menuitem>
+				</menu>
+			</menuitem>
+      <menuitem><a>Overall</a></menuitem>
+		</menu>
+	</nav>
+
     <div class="mainuser">
       <div class="skinimg">
         <canvas id="skin_container"></canvas>
@@ -408,6 +450,122 @@ place-items: center;
 }
 .Statbutton {
   top:-4.5em;
+}
+
+/* nav bar */
+html, body{
+   padding:0px;
+   margin:0px;
+   background:#191A1D;
+   font-family: 'Karla', sans-serif;
+   width:100vw;
+}
+body * {
+   margin:0;
+   padding:0;
+}
+
+/* HTML Nav Styles */
+/* HTML Nav Styles */
+/* HTML Nav Styles */
+nav menuitem {
+   position:relative;
+   display:block;
+   opacity:0;
+   cursor:pointer;
+}
+
+nav menuitem > menu {
+   position: absolute;
+   pointer-events:none;
+}
+nav > menu { display:flex; }
+
+nav > menu > menuitem { pointer-events: all; opacity:1; }
+menu menuitem a { white-space:nowrap; display:block; }
+   
+menuitem:hover > menu {
+   pointer-events:initial;
+}
+menuitem:hover > menu > menuitem,
+menu:hover > menuitem{
+   opacity:1;
+}
+nav > menu > menuitem menuitem menu {
+   transform:translateX(100%);
+   top:0; right:0;
+}
+/* User Styles Below Not Required */
+/* User Styles Below Not Required */
+/* User Styles Below Not Required */
+
+nav { 
+   margin-top: 40px;
+   margin-left: 40px;
+   z-index: 100;
+   float:right;
+}
+
+nav a {
+   background:#75F;
+   color:#FFF;
+   min-width:190px;
+   transition: background 0.5s, color 0.5s, transform 0.5s;
+   margin:0px 6px 6px 0px;
+   padding:20px 40px;
+   box-sizing:border-box;
+   border-radius:3px;
+   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+   position:relative;
+}
+
+nav a:hover:before {
+   content: '';
+   top:0;left:0;
+   position:absolute;
+   background:rgba(0, 0, 0, 0.2);
+   width:100%;
+   height:100%;
+}
+
+nav > menu > menuitem > a + menu:after{
+   content: '';
+   position:absolute;
+   border:10px solid transparent;
+   border-top: 10px solid white;
+   left:12px;
+   top: -40px;  
+}
+nav menuitem > menu > menuitem > a + menu:after{ 
+   content: '';
+   position:absolute;
+   border:10px solid transparent;
+   border-left: 10px solid white;
+   top: 20px;
+   left:-180px;
+   transition: opacity 0.6, transform 0s;
+}
+
+nav > menu > menuitem > menu > menuitem{
+   transition: transform 0.6s, opacity 0.6s;
+   transform:translateY(150%);
+   opacity:0;
+}
+nav > menu > menuitem:hover > menu > menuitem,
+nav > menu > menuitem.hover > menu > menuitem{
+   transform:translateY(0%);
+   opacity: 1;
+}
+
+menuitem > menu > menuitem > menu > menuitem{
+   transition: transform 0.6s, opacity 0.6s;
+   transform:translateX(195px) translateY(0%);
+   opacity: 0;
+} 
+menuitem > menu > menuitem:hover > menu > menuitem,  
+menuitem > menu > menuitem.hover > menu > menuitem{  
+   transform:translateX(0) translateY(0%);
+   opacity: 1;
 }
 
 /* From uiverse.io by @satyamchaudharydev */
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,11 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 import user from '../components/user.vue'
 import home from '../components/home.vue'
-import skintest from '../components/skintest.vue'
 import Name404 from '../components/404.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: home},
     {path: '/user/:username', name: 'User', component: user},
-    {path: '/skintest', name: 'skintest', component: skintest},
     {path: '/:pathMatch(.*)*', name: '404', component: Name404}
 ]
 
